22d20e00945ecc9533dcf2b9ead6901dd4a93874
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2021-08-20 10:20:44 +0700

    get list

---
+++ src/main/java/codegym/vn/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package codegym.vn.service;
+
+import codegym.vn.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
